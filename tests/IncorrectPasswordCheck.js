891cbbb0efdbf6343af45160f136be7e738a2b9a
evgenija drapun <evgenija.dr@gmail.com>
2015-10-21 12:50:23 +0300

    Jane: Added sign up test with new scenario approach

---
+++ tests/IncorrectPasswordCheck.js
@@ -0,0 +1,34 @@
+var SignUpPage = require('../pages/SignUpPage.js');
+var RandomUtils = require('../utils/RandomUtils.js');
+
+describe('Incorrect password during sign up check', function(){
+    var signUpPage = new SignUpPage();
+    var randomUtilss = new RandomUtils();
+
+    it('Password cannot be less than 8 chars', function(){
+        signUpPage.openSignUpPage();
+
+        var password = "B" + randomUtilss.randomizeString(5) + "1";
+        signUpPage.setPassword(password);
+
+        expect(signUpPage.gerErrorOfIncorrectPassword()).toEqual(signUpPage.getErrorMessages().incorrectPassword);
+    });
+
+    it('Password should contain at least one UpperCase char', function() {
+        signUpPage.openSignUpPage();
+
+        var password = randomUtilss.randomizeString(7).toLowerCase() + "1";
+        signUpPage.setPassword(password);
+        browser.pause();
+        expect(signUpPage.gerErrorOfIncorrectPassword()).toEqual(signUpPage.getErrorMessages().incorrectPassword);
+    });
+
+    it('Password should contain at least one numeric char', function() {
+        signUpPage.openSignUpPage();
+
+        var password = randomUtilss.randomizeAlphaString(7).toLowerCase() + "B";
+        signUpPage.setPassword(password);
+
+        expect(signUpPage.gerErrorOfIncorrectPassword()).toEqual(signUpPage.getErrorMessages().incorrectPassword);
+    });
+});
