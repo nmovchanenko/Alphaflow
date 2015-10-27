04eb68de5f15859d9b8ba3466813a80c643ac8d5
mikalai.mauchanenka <movchanenko.nn@gmail.com>
2015-10-27 16:05:16 +0300

    commit for testing logger in JenkinsCI

--- conf.js
+++ conf.js
@@ -29,8 +29,8 @@ var reporter = new HtmlReporter({
         var currentDate = new Date(),
             currentHoursIn24Hour = currentDate.getHours(),
             currentTimeInHours = currentHoursIn24Hour>12? currentHoursIn24Hour-12: currentHoursIn24Hour,
-            totalDateString = currentDate.getDate()+'-'+ monthMap[currentDate.getMonth()+1]+ '-'+(currentDate.getYear()+1900) +
-                '-'+ currentTimeInHours+'h-' + currentDate.getMinutes()+'m';
+            totalDateString = currentDate.getDate()+'-'+ monthMap[currentDate.getMonth()+1]+ '-'+(currentDate.getYear()+1900);
+                //+ '-'+ currentTimeInHours+'h-' + currentDate.getMinutes()+'m';
 
         return path.join(totalDateString,capabilities.caps_.browserName, descriptions.join('-'));
     }
