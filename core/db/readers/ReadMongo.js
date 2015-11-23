e2907a343e0424759ba8d79ac0d9770829a92c3c
mikalai.mauchanenka <movchanenko.nn@gmail.com>
2015-11-23 17:39:08 +0300

    Added writing data to json file

--- core/db/readers/ReadMongo.js
+++ core/db/readers/ReadMongo.js
@@ -2,14 +2,14 @@ var User = require('../model/UserSchema.js'),
     co = require('co'),
     jsonfile = require('jsonfile');
 
-var file = '../../../resources/MongoData.json';
+var outputFile = '../../../resources/MongoData.json';
 
 co(function*() {
     // test for local db
     var user1 = yield User.find({'name.first': 'Evgenija'});
     console.log(user1);
 
-    jsonfile.writeFileSync(file, user1, {spaces: 2});
+    jsonfile.writeFileSync(outputFile, user1, {spaces: 2});
 
     process.exit(0);
 })();
