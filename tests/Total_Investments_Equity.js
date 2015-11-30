159486b70af5f3743a77b6a1b35561b4221558f9
mikalai.mauchanenka <movchanenko.nn@gmail.com>
2015-11-30 18:28:22 +0300

    Using "yield" in 'Total_Investment_Equity' test

--- tests/Total_Investments_Equity.js
+++ tests/Total_Investments_Equity.js
@@ -1,31 +1,33 @@
 var Earning         = require('../common/Earning.js'),
     config          = require('../settings'),
     RealEstateTable = require("../pages/components/RealEstateTable.js"),
-    RealEstatePage  = require("../pages/RealEstatePage.js");
+    RealEstatePage  = require("../pages/RealEstatePage.js"),
+    co              = require('co');
 
 describe('Total Investments', function () {
     var realEstatePage = new RealEstatePage();
     var realEstateTable = new RealEstateTable();
 
     it('calculate Equity', function () {
-        step.loginAs(config.get('app:login'), config.get('app:password'));
-        step.openRealEstate();
 
-        realEstatePage.filterInvestmentsByType("Equity");
-        realEstatePage.turnOffFilterByStatus();
+        co(function * () {
+            step.loginAs(config.get('app:login'), config.get('app:password'));
+            step.openRealEstate();
 
-        realEstateTable.getInvestmentsCollection().then(investments => {
-            "use strict";
+            realEstatePage.filterInvestmentsByType("Equity");
+            realEstatePage.turnOffFilterByStatus();
+
+            var investments = yield realEstateTable.getInvestmentsCollection();
+
             var sumInvestment = 0;
-
             for (var investment of investments.values()) {
                 sumInvestment += investment.getAmount();
             }
-            realEstatePage.getTotalInvestmentsEquity().then(totalEquity => {
-                // we need to parse totalEquity as 'sumInvestment' is float
-                expect(parseFloat(totalEquity)).toEqual(Math.round(sumInvestment));
-            })
-        });
+            var totalEquity = yield realEstatePage.getTotalInvestmentsEquity();
+
+            // we need to parse totalEquity as 'sumInvestment' is float
+            expect(parseFloat(totalEquity)).toEqual(Math.round(sumInvestment));
+        })();
 
     });
 });
